7bfb5592734f29ec9c75fcaf29d58df86655c31c
Dzhurenovv <149161645+Dzhurenovv@users.noreply.github.com>
2023-10-27 13:55:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Double_arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = { { 1, 2, 3 }, { 4, 5, 6 } };
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
